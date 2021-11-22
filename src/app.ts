53e36a1631dcaf3e7c0cc429f8d47969f7053dbe
BrianDavidYork <briandavidyork@gmail.com>
2021-11-21 20:37:57 -0500

    Move Procfile; Port;

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ const express = require( "express" )
 const app = express()
 const bp = require('body-parser')
 const controller = require('./controller')
-const port = 8080
+const port = process.env.PORT || 8080
 
 app.listen(port, () => {
   console.log('Server running on port ' + port)
